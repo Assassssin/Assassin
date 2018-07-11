af706ab98290202fdcef17e4c05f276a430a5972
Whalesharker <Ian@micrognomics.com>
2018-07-11 11:43:03 -0400

    Fixed about page.

--- pages/about/client/about.js
+++ pages/about/client/about.js
@@ -1,9 +1,11 @@
 const team =
   [
-    {name:"Tim Hickey"    , email: "tjhickey@brandeis.edu" },
-    {name:"Jordan Phillips", email:"jordan.m.phillips23@gmail.com"},
-    {name:"Zeline Bartolome", email:"ztbartolome@brandeis.edu"},
-    {name:"Dongwei Wu", email:"??"}
+    {name:"Ian"    , email: "Orcas" },
+    {name:"Felix", email:"??"},
+    {name:"Tani", email:"?"},
+    {name:"Brad", email:"??"},
+    {name:"Josh", email:"??"}
+
   ];
 
 Template.about.helpers(
