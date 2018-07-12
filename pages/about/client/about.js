e95ad97c07b13d48cbc24aefd825507781795cae
Felix Hasson <turdfacexxx7@gmail.com>
2018-07-11 20:22:44 -0400

    afs

--- pages/about/client/about.js
+++ pages/about/client/about.js
@@ -4,7 +4,6 @@ const team =
     {name:"Felix", email:"??"},
     {name:"Tani", email:"?"},
     {name:"Josh", email:"??"}
-    name: <a href="https://www.youtube.com/embed/Ikxc0aMZVXk?controls=0&amp;showinfo=0&amp;ecver=1?start=57&end=68">Brad(formerly)</a>,email: "??"
   ];
 
 Template.about.helpers(
