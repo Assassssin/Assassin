98719e5e43b26a2dafc81d5b148e30cbb12e92c3
Whalesharker <Ian@micrognomics.com>
2018-07-12 09:21:57 -0400

    Revert "Fixxed link"
    This reverts commit 797c9754125df8553dd78e2b911a02ffc735223b.

--- pages/about/client/about.js
+++ pages/about/client/about.js
@@ -3,8 +3,14 @@ const team =
     {name:"Ian"    , email: "Orcas" },
     {name:"Felix", email:"??"},
     {name:"Tani", email:"?"},
+<<<<<<< HEAD
     {name:"Josh", email:"??"},
     {name: <a href="https://www.youtube.com/embed/Ikxc0aMZVXk?start=62&end=68">Brad(formerly)</a>,email: "??"}
+=======
+    {name:"Brad", email:"??"},
+    {name:"Josh", email:"??"}
+
+>>>>>>> parent of 797c975... Fixxed link
   ];
 
 Template.about.helpers(
