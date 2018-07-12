32e1e05e387532fecf8a7f3ae9f00e1fab6fd90a
Ian <Ian@micrognomics.com>
2018-07-11 20:00:52 -0400

    Added appropiate video link to Brad. Also readded Brad.

--- pages/about/client/about.js
+++ pages/about/client/about.js
@@ -3,9 +3,8 @@ const team =
     {name:"Ian"    , email: "Orcas" },
     {name:"Felix", email:"??"},
     {name:"Tani", email:"?"},
-    {name:"Brad", email:"??"},
     {name:"Josh", email:"??"}
-
+    name: <a href="https://www.youtube.com/embed/Ikxc0aMZVXk?controls=0&amp;showinfo=0&amp;ecver=1?start=57&end=68">Brad(formerly)</a>,email: "??"
   ];
 
 Template.about.helpers(
